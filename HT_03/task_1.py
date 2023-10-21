38aa2ed558b731eeec76eca1c56610b5539694b9
Andrii <itehnolog@gmail.com>
2023-10-21 20:42:02 +0300

    Tasks 1 and 6 changed

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -3,14 +3,15 @@ The list of tuples can be hardcoded. The "replacement" value is entered by user.
 The number of elements in the tuples must be different.
 """
 
-list_of_tuples = [(1, 2, 3), ('a', 'b', 'c', 'd'), (2.3, 3.6, 8.3, 5.8, 9.7, 10.0)]
+list_of_tuples = [(), (1, 2, 3), ('a', 'b', 'c', 'd'), (2.3, 3.6, 8.3, 5.8, 9.7, 10.0)]
 
 value_for_replacement = input("Please enter value for replacement: ")
 counter = 0
 for i in list_of_tuples:
-    temp_list = list(i)
-    temp_list[len(temp_list) - 1] = value_for_replacement
-    list_of_tuples[counter] = tuple(temp_list)
+    if i:
+        temp_list = list(i)
+        temp_list[len(temp_list) - 1] = value_for_replacement
+        list_of_tuples[counter] = tuple(temp_list)
     counter += 1
 print(f'New list of tuples is:\n{list_of_tuples}')
 
--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,13 +1,43 @@
 """Write a script to get the maximum and minimum value in a dictionary."""
 
-import random
 
-length_of_dict = random.randint(10, 20)
-dict_of_numbers = dict()
-for i in range(length_of_dict):
-    dict_of_numbers[i] = random.randint(0, 1000)
-print(dict_of_numbers)
-print(f"Maximum value of dict is: {max(dict_of_numbers.values())}")
-print(f"Minimum value of dict is: {min(dict_of_numbers.values())}")
+def maximum_of_list(list_of_elements):
+    if list_of_elements:
+        return max(list_of_elements)
+    else:
+        return 'not found'
 
 
+def minimum_of_list(list_of_elements):
+    if list_of_elements:
+        return min(list_of_elements)
+    else:
+        return 'not found'
+
+
+sample_dict = {'foo': '36',
+               'bar': [-3],
+               'dou': {"value1": 77},
+               'USD': 36,
+               'AUD': 19.2,
+               'name': 99.99,
+               'age': 100,
+               'baz': [9, 6],
+               'test': ("value", 150),
+               }
+max_lists = [[], [], [], []]
+names_of_lists = ['numbers', 'strings', 'lists', 'tuples']
+for value in sample_dict.values():
+    if type(value) == int or type(value) == float:
+        max_lists[0].append(value)
+    elif type(value) == str:
+        max_lists[1].append(value)
+    elif type(value) == list:
+        max_lists[2].append(value)
+    elif type(value) == tuple:
+        max_lists[3].append(value)
+count = 0
+for elem in max_lists:
+    print(f"Maximum of {names_of_lists[count]} is {maximum_of_list(elem)}")
+    print(f"Minimum of {names_of_lists[count]} is {minimum_of_list(elem)}")
+    count += 1
